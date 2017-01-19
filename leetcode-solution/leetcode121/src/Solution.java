7d4173cee919279d007712d3ce9d8e70ebac1886
Felicity Liu <FelicityRooting@users.noreply.github.com>
2017-01-19 17:12:54 -0500

    Update Solution.java

--- leetcode-solution/leetcode121/src/Solution.java
+++ leetcode-solution/leetcode121/src/Solution.java
@@ -5,17 +5,17 @@
 // 当天价格出售的收益，作为可能的最大收益，整个遍历过程中，出现过的最大收益就是所求
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length < 2) return 0;
-
-        int maxProfit = 0;
-        int curMin = prices[0];
-
-        for (int i = 1; i < prices.length; i++) {
-            curMin = Math.min(curMin, prices[i]);
-            maxProfit = Math.max(maxProfit, prices[i] - curMin);
+        if (prices.length < 2) return 0;//如果价格只有一天的，则return 0，因为只有那一天也只能买入那一天，无profit
+        int maxProfit = 0;//建立最大利润变量
+        int curMin = prices[0];//将第0天的price设为当前最小的买入价格
+        for (int i = 1; i < prices.length; i++) {//遍历整个天数
+            curMin = Math.min(curMin, prices[i]);//找出所有天数里买入价格最低的
+            maxProfit = Math.max(maxProfit, prices[i] - curMin);//找出当天价格减去最小买入价格后得到的最大利润，若前一个maxprofit更大，则不变，
+            //否则用新得出的利润替换原来的利润
+            
         }
 
-        return maxProfit;
+        return maxProfit;//返回最大利润
         }
     }
 }
