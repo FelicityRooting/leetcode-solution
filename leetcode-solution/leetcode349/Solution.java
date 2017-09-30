0c2aa3e2fb89a6f69ae44009abcd9a2ddeb19a93
Felicity Liu <FelicityRooting@users.noreply.github.com>
2017-09-29 21:02:46 -0400

    add leetcode349

---
+++ leetcode-solution/leetcode349/Solution.java
@@ -0,0 +1,66 @@
+//  Given two arrays, write a function to compute their intersection.
+
+// Example:
+// Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+// Note:
+
+//     Each element in the result must be unique.
+//     The result can be in any order.
+public class Solution {
+    
+    /*
+     * @param nums1: an integer array
+     * @param nums2: an integer array
+     * @return: an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        // write your code here
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        
+        Arrays.sort(nums1);
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                continue;
+            }
+            if (binarySearch(nums1, nums2[i])) {
+                set.add(nums2[i]);
+            }
+        }
+        
+        int[] results = new int[set.size()];
+        int index = 0;
+        for (Integer num : set) {
+            results[index++] = num;
+        }
+        return results;
+    }
+    
+    private boolean binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] == target) {
+            return true;
+        }
+        if (nums[end] == target) {
+            return true;
+        }
+        return false;
+    }
+};
