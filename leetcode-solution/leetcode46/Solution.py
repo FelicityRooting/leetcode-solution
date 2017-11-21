70a32329f9174addf7899455efeb2a8c7a74b8f1
Felicity Liu <FelicityRooting@users.noreply.github.com>
2017-11-20 21:05:27 -0500

    modify leetcode46 solution.py

--- leetcode-solution/leetcode46/Solution.py
+++ leetcode-solution/leetcode46/Solution.py
@@ -4,7 +4,7 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums = sorted(nums)#就差这一步很关键，除了这里，其他的和permutation那题一样
+        nums = sorted(nums)
         if len(nums) == 0:
             return [[]]
         if len(nums) == 1:
